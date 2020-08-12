5a692a428d703775190aa0bf16092ea8741ad34b
Mateus Moura Santos <msantos.96@outlook.com>
2020-08-12 15:34:53 -0300

    express configurado

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: process.env.CORSORIGIN || "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+    console.log("get on '/'");
+    response.json({message: "Hello World"});
+
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=>{
+    console.log(`Server rodando na porta ${PORT}`);
+});
