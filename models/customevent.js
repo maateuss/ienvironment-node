53c9cf4a862a1d6be01cea84df81b27b69c29406
Mateus Moura Santos <msantos.96@outlook.com>
2020-09-22 20:32:04 -0300

    customevent  model added

---
+++ models/customevent.js
@@ -0,0 +1,26 @@
+
+
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+        {
+            targets: [Schema.Types.ObjectId],
+            operator: String,
+            description: String,
+            topic: String
+        },
+        {
+            timestamps: true
+        }
+    );
+    
+    schema.method("toJSON", function(){
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    
+    const CustomEvent = mongoose.model("customevent", schema);
+        
+    return CustomEvent;
+}
