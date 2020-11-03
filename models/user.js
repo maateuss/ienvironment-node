f66dd9d70b6594e96c7ce815aabfb03daa4a745e
Mateus Moura Santos <msantos.96@outlook.com>
2020-11-02 21:43:24 -0300

    token expiration time added

--- models/user.js
+++ models/user.js
@@ -7,7 +7,6 @@ module.exports = mongoose => {
             email: String,
             login: { type: String, required: true, index: {unique: true}},
             password: {type: String, required: true},
-            active: Boolean,
             enabled: {type: Boolean, default: false}
         },
         {
